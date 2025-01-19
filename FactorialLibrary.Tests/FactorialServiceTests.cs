36af30c557bd9a9737608284b0048097df42e8bc
tmmht <romaton.9.moko@gmail.com>
2025-01-19 08:05:31 +0000

    Fix FactorialService.cs, FactorialServiceTests.cs

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -8,4 +8,10 @@ public class FactorialServiceTests
     var result = FactorialService.Factorial(0);
     Assert.Equal(1,result);
   }
+  public void Test_negative()
+  {
+    Assert.Throws<InvalidOperationException>(()=>{
+      var result = FactorialService.Factorial(-1);
+    });
+  }
 }
